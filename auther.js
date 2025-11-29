1744dc02587ae72c92dcd0595d96acbfff181208
—͟͞𝗔𝗕𝗗𝗨𝗟𝗟𝗔𝗛 𝗠𝗘𝗧𝗛𝗢𝗗 <abdullah04913@gmail.com>
2025-11-29 23:11:35 +0500

    Update auther.js

--- auther.js
+++ auther.js
@@ -1,69 +1,41 @@
-/*document.addEventListener('DOMContentLoaded', function() {
-        var allowedURLs = [
-"https://orgmovies.store",  
-                "https://9xflix.ch",
-                "https://filmyfly.lu",
-                "https://filmy-fly.pl",
-                "https://filmyfly.es",
-                "https://filmyfly.at",
-                "https://moviesbake.online",
-                "https://cinema4free.online",
-                "https://bollypro.sbs/",
- "https://biographyhero.lovestoblog.com",
-                "https://filmyfly.pl",
-                "https://demo-mhp.blogspot.com",
-                "https://movies4u.press",
-                "https://nayaharyana-com.preview-domain.com",
- "https://vegaflix.com",
-                "https://www.zcflick.in",
-                "http://Movies4u.press",
-                "https://www.cineflim.com",
-                "https://hub4umovei.com",
-                "http://movies4u24.in",
-"https://www.boomfilms.xyz", 
-  "https://cinema4free.xyz",
-    "https://www.mkv.icu", 
-                "https://www.akayamovie.xyz",
-                "https://exprothikhad.blogspot.com",
- "https://vegamovies.mywire.org",
- "https://hdhub4uui.blogspot.com",     
-   "https://afilmy4wap.org",
-                "https://plusuiteme.blogspot.com",
-"https://solution-manualspdf.blogspot.com",  
-                "https://www.timestoryaxom.in",
-                 "https://www.myanimehindi.in",
-                "https://girgit-ui.blogspot.com",
-   "https://www.filmyslap.xyz", 
-                 "https://codingrider.blogspot.com",
-                "https://www.moviehubplay.in",
-                "https://hdhub4u-clone.blogspot.com",
-                "https://neuplayer.blogspot.com"
-];
-    
-});*/
+var lazyanalisis = !1;
 
-/*(function(s,u,z,p){s.src=u,s.setAttribute('data-zone',z),p.appendChild(s);})(document.createElement('script'),'https://al5sm.com/tag.min.js',9415867,document.body||document.documentElement)
-*/
-var lazyanalisis=!1;window.addEventListener("scroll",function(){(0!=document.documentElement.scrollTop&&!1===lazyanalisis||0!=document.body.scrollTop&&!1===lazyanalisis)&&(!function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src="https://www.googletagmanager.com/gtag/js?id=G-2VYRMPXK0F";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(e,a)}(),lazyanalisis=!0)},!0);
+window.addEventListener("scroll", function () {
+    if ((document.documentElement.scrollTop != 0 && lazyanalisis === !1) ||
+        (document.body.scrollTop != 0 && lazyanalisis === !1)) {
 
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'G-2VYRMPXK0F');
+        (function () {
+            var e = document.createElement("script");
+            e.type = "text/javascript";
+            e.async = true;
+            e.src = "https://www.googletagmanager.com/gtag/js?id=G-2VYRMPXK0F";
+            var a = document.getElementsByTagName("script")[0];
+            a.parentNode.insertBefore(e, a);
+        })();
+
+        lazyanalisis = !0;
+    }
+}, true);
+
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+gtag("js", new Date());
+gtag("config", "G-2VYRMPXK0F");
 
 setTimeout(() => {
-  const newApiKey = "ea96bc4942aa3d3737f7d767f7d9c6f2704a391c";
+    const newApiKey = "ea96bc4942aa3d3737f7d767f7d9c6f2704a391c";
 
-  document.querySelectorAll('a[href*="dashboard.smallshorts.com/full"]').forEach(link => {
-    try {
-      const url = new URL(link.href);
-      if (url.hostname === "dashboard.smallshorts.com" && url.pathname === "/full") {
-        if (url.searchParams.has("api")) {
-          url.searchParams.set("api", newApiKey);
-          link.href = url.toString();
-        }
-      }
-    } catch (e) {
-     }
-  });
+    document.querySelectorAll('a[href*="dashboard.smallshorts.com/full"]').forEach(link => {
+        try {
+            const url = new URL(link.href);
+
+            if (url.hostname === "dashboard.smallshorts.com" && url.pathname === "/full") {
+                if (url.searchParams.has("api")) {
+                    url.searchParams.set("api", newApiKey);
+                    link.href = url.toString();
+                }
+            }
+
+        } catch (e) {}
+    });
 }, 1000);
